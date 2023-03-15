79c528e5dd3ddf1aef684b954dc559ac0b10ab81
Joy Ashidi <106686496+joyashidi@users.noreply.github.com>
2023-03-15 14:34:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var hamburger = document.querySelector(".ham");
+var navlist = document.querySelector(".nav-list");
+var links = document.querySelector(".nav-list li");
+
+hamburger.addEventListener("click", function(){
+    this.classList.toggle("click");
+    navlist.classList.toggle("open");
+})
+
